447b0b91a4f62ee2c0a990c7f38f93d42116906f
Surya Kant Bansal <skb1129@yahoo.com>
2018-12-14 18:37:57 +0530

    setup App.js

--- Example/App.js
+++ Example/App.js
@@ -7,23 +7,19 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { changeIcon } from 'react-native-change-icon';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableOpacity onPress={() => changeIcon('Checked')}>
+          <Text style={styles.button}>SWITCH TO CHECKED ICON</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => changeIcon('Cancel')}>
+          <Text style={styles.button}>SWITCH TO CANCEL ICON</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -32,18 +28,13 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  button: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
