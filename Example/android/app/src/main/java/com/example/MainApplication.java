7749b358326e87cf7c782e5281d4e4d54964cc1b
Surya Kant Bansal <skb1129@yahoo.com>
2018-12-14 18:18:11 +0530

    Example android setup

--- Example/android/app/src/main/java/com/example/MainApplication.java
+++ Example/android/app/src/main/java/com/example/MainApplication.java
@@ -14,6 +14,8 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  public static List<String> ICONS = Arrays.<String>asList("Checked", "Cancel");
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -23,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNChangeIconPackage()
+        new MainReactPackage(),
+        new RNChangeIconPackage(BuildConfig.APPLICATION_ID, MainApplication.ICONS)
       );
     }
 
