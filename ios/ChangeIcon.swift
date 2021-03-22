05f988f35bdad89e6af1be0e6d1faab6e8518a7c
Surya Kant Bansal <suryakantbansal97@gmail.com>
2021-03-22 20:24:33 +0530

    fix: requiresMainQueueSetup warn fix

--- ios/ChangeIcon.swift
+++ ios/ChangeIcon.swift
@@ -1,6 +1,11 @@
 @objc(ChangeIcon)
 class ChangeIcon: NSObject {
 
+    @objc
+    static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
+
     @available(iOS 10.3, *)
     @objc(changeIcon:withResolver:withRejecter:)
     func changeIcon(iconName: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
