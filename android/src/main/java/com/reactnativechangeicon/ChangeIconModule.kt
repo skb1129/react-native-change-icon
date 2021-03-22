1e69cfaee6cf7a23cdaf733088aa243d48eeb8c2
ensargunesdogdu <ensargunesdogdu@gmail.com>
2021-03-22 14:28:18 +0300

    Android new logic (#28)
    * Android Icon Change Bug fixed for Android 9 or Higher.
    
    * Imports Fixed.
    
    * Bug Fix
    
    * Import Fix

--- android/src/main/java/com/reactnativechangeicon/ChangeIconModule.kt
+++ android/src/main/java/com/reactnativechangeicon/ChangeIconModule.kt
@@ -1,15 +1,19 @@
 package com.reactnativechangeicon
 
 import android.app.Activity
+import android.app.Application
 import android.content.pm.PackageManager
 import android.content.ComponentName
+import android.os.Bundle
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
-class ChangeIconModule(reactContext: ReactApplicationContext, private val packageName: String) : ReactContextBaseJavaModule(reactContext) {
+class ChangeIconModule(reactContext: ReactApplicationContext, private val packageName: String) : ReactContextBaseJavaModule(reactContext), Application.ActivityLifecycleCallbacks {
+    private var classToKill: String = ""
+    private var iconChanged: Boolean = false;
     private var componentClass: String = ""
     override fun getName(): String {
         return "ChangeIcon"
@@ -30,15 +34,50 @@ class ChangeIconModule(reactContext: ReactApplicationContext, private val packag
         }
         promise.resolve(true)
         activity.packageManager.setComponentEnabledSetting(
-                ComponentName(packageName, activeClass),
-                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                PackageManager.DONT_KILL_APP
+            ComponentName(packageName, activeClass),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
         )
-        activity.packageManager.setComponentEnabledSetting(
-                ComponentName(packageName, componentClass),
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-        )
+        classToKill = componentClass;
         componentClass = activeClass
+        activity.application.registerActivityLifecycleCallbacks(this)
+        iconChanged = true;
+    }
+
+    private fun completeIconChange() {
+        if (iconChanged) {
+          val activity: Activity? = currentActivity
+          if (activity == null) {
+            return
+          }
+          activity.packageManager.setComponentEnabledSetting(
+            ComponentName(packageName, classToKill),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+          )
+          iconChanged = false;
+        }
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+        completeIconChange();
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
     }
 }
