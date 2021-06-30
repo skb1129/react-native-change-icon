f16ab98122e688364fd94e561c97a07c7180b917
gossamr <git@gossamr.org>
2021-06-30 18:01:01 +0700

    fix: add support to reset iOS icon to primary, run on main thread

--- ios/ChangeIcon.swift
+++ ios/ChangeIcon.swift
@@ -8,17 +8,19 @@ class ChangeIcon: NSObject {
 
     @available(iOS 10.3, *)
     @objc(changeIcon:withResolver:withRejecter:)
-    func changeIcon(iconName: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        if !UIApplication.shared.supportsAlternateIcons {
-            reject("Error", "Alternate icon not supported", nil)
-            return
+    func changeIcon(iconName: String?, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+         DispatchQueue.main.async {
+            if !UIApplication.shared.supportsAlternateIcons {
+                reject("Error", "Alternate icon not supported", nil)
+                return
+            }
+            let currentIcon = UIApplication.shared.alternateIconName
+            if iconName == currentIcon {
+                reject("Error", "Icon already in use", nil)
+                return
+            }
+            resolve(true)
+            UIApplication.shared.setAlternateIconName(iconName)
         }
-        let currentIcon = UIApplication.shared.alternateIconName
-        if iconName == currentIcon {
-            reject("Error", "Icon already in use", nil)
-            return
-        }
-        resolve(true)
-        UIApplication.shared.setAlternateIconName(iconName)
     }
 }
