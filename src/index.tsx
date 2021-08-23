efb25947b6d9cb88a9a4cdf501aa478cdd881514
Jakub <jakub.gonet@swmansion.com>
2021-08-23 16:32:21 +0200

    Update TS type for changeIcon function (#38)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { NativeModules } from 'react-native';
 
-const changeIcon = (iconName: string) =>
+const changeIcon = (iconName: string): Promise<string> =>
   NativeModules.ChangeIcon.changeIcon(iconName);
 
 export { changeIcon };
